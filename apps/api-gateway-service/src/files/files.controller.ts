c3dae7a377c94b5113e1ff133365b2aada1260d2
Laurent Ouma <lawiomosh3@gmail.com>
2025-09-13 12:43:55 +0300

    Refactor FilesController to implement file upload logic for multiple files, including error handling and metadata storage. Remove unused file type validation and streamline session handling. Add TODOs for future enhancements on file deduplication and validation before uploads.

--- apps/api-gateway-service/src/files/files.controller.ts
+++ apps/api-gateway-service/src/files/files.controller.ts
@@ -39,6 +39,7 @@ import { S3Service } from '../s3/s3.service';
 import { AuthGuard, Session, UserSession } from '@thallesp/nestjs-better-auth';
 
 // TODO: implement deduplication of files by generating file hash and cross checking on dab if exist then retuern reference
+// Also implement methods to validate before uploading to bucket
 
 @UseGuards(AuthGuard)
 @Controller('files')
@@ -100,16 +101,11 @@ export class FilesController {
   })
   @Post('upload/single')
   async uploadSingleFile(
-    @Session() { session, user }: UserSession,
+    @Session() { user }: UserSession,
     @UploadedFile(
       'file',
       new ParseFilePipeBuilder()
         .addValidator(new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 })) // 10MB
-        // .addValidator(
-        //   new FileTypeValidator({
-        //     fileType: /\.(jpg|jpeg|png|gif|pdf|doc|docx|txt)$/,
-        //   }),
-        // )
         .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
     )
     file: Express.Multer.File,
@@ -178,25 +174,70 @@ export class FilesController {
   })
   @ApiConsumes('multipart/form-data')
   @UseInterceptors(FilesInterceptor('files'))
-  uploadMultipleFile(
+  async uploadMultipleFile(
     @UploadedFiles(
       new ParseFilePipeBuilder()
         .addValidator(new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 })) // 10MB per file
-        .addValidator(
-          new FileTypeValidator({
-            fileType: /\.(jpg|jpeg|png|gif|pdf|doc|docx|txt)$/,
-          }),
-        )
         .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
     )
     files: Array<Express.Multer.File>,
+    @Session() { user }: UserSession,
     @Query() query: CustomRepresentationQueryDto,
     @Body() uploadFileDto: UploadMutipleFilesDto,
   ) {
-    // TODO: implement
-    console.log(files);
-    console.log(uploadFileDto);
-    throw new Error('Methoid not Implemented');
+    try {
+      const s3FileMetadata = await this.s3Service.uploadMultipleFiles(
+        files,
+        uploadFileDto.uploadTo,
+        uploadFileDto.isPublic,
+        {},
+      );
+
+      const uploadTasks = s3FileMetadata.files.map((file, index) =>
+        lastValueFrom(
+          this.fileService.file.createFile({
+            queryBuilder: query,
+            filename: file.filename,
+            hash: this.generateFileHash(files[index].buffer),
+            mimeType: file.contentType,
+            originalName: file.originalName,
+            purpose: uploadFileDto.purpose,
+            relatedModelId: uploadFileDto.relatedModelId,
+            relatedModelName: uploadFileDto.relatedModelName,
+            size: file.size.toString(),
+            storages: [
+              {
+                provider: CreateFileStorage_StorageProviders.LOCAL,
+                remoteId: file.id,
+                storagePath: uploadFileDto.uploadTo,
+                storageUrl: file.url ?? file.signedUrl,
+              },
+            ],
+            tags: uploadFileDto.tags?.split(',')?.map((t) => t.trim()),
+            uploadedById: user.id,
+            metadata: JSON.stringify(file.customMetadata ?? {}),
+            organizationId: undefined,
+            expiresAt: undefined, // TODO Future impl
+            lastAccessedAt: undefined, // TODO Future impl
+          }),
+        ),
+      );
+      const res = await Promise.allSettled(uploadTasks);
+      const success = res
+        .filter((r) => r.status === 'fulfilled')
+        .map((r) => r.value.data);
+      return success;
+    } catch (error) {
+      this.logger.error(`S3 upload failed : ${error.message}`, error.stack);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        `Upload failed: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
   @Post('upload/multiple/fields')
   @ApiConsumes('multipart/form-data')
@@ -206,7 +247,7 @@ export class FilesController {
       'Upload files from different form fields to the specified location',
   })
   @UseInterceptors(AnyFilesInterceptor())
-  uploadFiles(
+  async uploadFiles(
     @UploadedFiles(
       new ParseFilePipeBuilder()
         .addValidator(new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 })) // 10MB per file
@@ -221,13 +262,63 @@ export class FilesController {
         }),
     )
     files: Array<Express.Multer.File>,
+    @Session() { user }: UserSession,
     @Query() query: CustomRepresentationQueryDto,
     @Body() uploadFileDto: UploadFilesDto,
   ) {
-    // TODO: implement
-    console.log(files);
-    console.log(uploadFileDto);
-    throw new Error('Methoid not Implemented');
+    try {
+      const s3FileMetadata = await this.s3Service.uploadMultipleFiles(
+        files,
+        uploadFileDto.uploadTo,
+        uploadFileDto.isPublic,
+        {},
+      );
+
+      const uploadTasks = s3FileMetadata.files.map((file, index) =>
+        lastValueFrom(
+          this.fileService.file.createFile({
+            queryBuilder: query,
+            filename: file.filename,
+            hash: this.generateFileHash(files[index].buffer),
+            mimeType: file.contentType,
+            originalName: file.originalName,
+            purpose: uploadFileDto.purpose,
+            relatedModelId: uploadFileDto.relatedModelId,
+            relatedModelName: uploadFileDto.relatedModelName,
+            size: file.size.toString(),
+            storages: [
+              {
+                provider: CreateFileStorage_StorageProviders.LOCAL,
+                remoteId: file.id,
+                storagePath: uploadFileDto.uploadTo,
+                storageUrl: file.url ?? file.signedUrl,
+              },
+            ],
+            tags: uploadFileDto.tags?.split(',')?.map((t) => t.trim()),
+            uploadedById: user.id,
+            metadata: JSON.stringify(file.customMetadata ?? {}),
+            organizationId: undefined,
+            expiresAt: undefined, // TODO Future impl
+            lastAccessedAt: undefined, // TODO Future impl
+          }),
+        ),
+      );
+      const res = await Promise.allSettled(uploadTasks);
+      const success = res
+        .filter((r) => r.status === 'fulfilled')
+        .map((r) => r.value.data);
+      return success;
+    } catch (error) {
+      this.logger.error(`S3 upload failed : ${error.message}`, error.stack);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+
+      throw new HttpException(
+        `Upload failed: ${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
   @Delete('/:id')
